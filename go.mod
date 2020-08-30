33dbadd5f8d65f16cba526a440e7fa7560f61cdb
hatobus <s1240056@gmail.com>
2020-08-31 02:16:07 +0900

    ownerとrepoを.gitファイルから取得する

--- go.mod
+++ go.mod
@@ -5,13 +5,13 @@ go 1.14
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.0 // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
-	github.com/google/go-github v17.0.0+incompatible // indirect
-	github.com/google/go-querystring v1.0.0 // indirect
-	github.com/martinssipenko/go-github v17.0.0+incompatible // indirect
+	github.com/google/go-github v17.0.0+incompatible
+	github.com/google/go-github/v31 v31.0.0 // indirect
 	github.com/urfave/cli v1.22.4
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
 	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
-	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
 	google.golang.org/appengine v1.6.6 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
 )
--- updator/client.go
+++ updator/client.go
@@ -2,22 +2,24 @@ package updator
 
 import (
 	"context"
+	"os"
+	"os/exec"
+	"strings"
+
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
 	"golang.org/x/xerrors"
-	"os"
 )
 
 var (
 	ErrClientGithubKeyNotFound = xerrors.New("github token not found")
-	ErrFailedToGetPublicKey = xerrors.New("failed to get public key")
+	ErrFailedToGetPublicKey    = xerrors.New("failed to get public key")
+	ErrFailedToGetOwnerOrRepo  = xerrors.New("failed to get owner or repo")
 )
 
 type GithubClient struct {
-	Token string
-	PubKey string
-	Owner string
-	Repo string
+	Token  string
+	PublicKey *github.PublicKey
 	Client *github.Client
 }
 
@@ -33,12 +35,47 @@ func NewGithubClient() (*GithubClient, error) {
 	oauthClient := oauth2.NewClient(context.Background(), sts)
 	githubClient := github.NewClient(oauthClient)
 
+	owner, repo, err := getOwnersAndRepoFromCurrentGitFile()
+	if err != nil {
+		return nil, err
+	} else if owner == "" || repo == "" {
+		return nil, ErrFailedToGetOwnerOrRepo
+	}
+
+	pubKey, _, err := githubClient.Actions.GetPublicKey(context.Background(), owner, repo)
+	if err != nil {
+		return nil, err
+	}
+
 	return &GithubClient{
-		Token: githubToken,
+		Token:  githubToken,
 		Client: githubClient,
+		PublicKey: pubKey,
 	}, nil
 }
 
-func (g *GithubClient) GetPublicKeyFromGitHub(owner, repo string) (string, error) {
-	panic("not impl")
+func getOwnersAndRepoFromCurrentGitFile() (string, string, error) {
+	out, err := exec.Command("git", "config", "--list").Output()
+	if err != nil {
+		return "", "", err
+	}
+
+	configs := strings.Split(string(out), "\n")
+
+	var owner, repo string
+
+	for _, s := range configs {
+		feature := strings.Split(s, "=")
+		key := feature[0]
+
+		switch(key){
+		case "user.name":
+			owner = feature[1]
+		case "remote.origin.url":
+			repo = feature[1]
+		default:
+		}
+	}
+
+	return owner, repo, nil
 }
